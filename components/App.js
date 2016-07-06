ea8e1cc7910b88b069878add10488982a9e69f3f
Petar Atanasov <petar.atanasov@botronsoft.com>
2016-07-06 12:43:59 +0300

    Completed tutorial part 3

--- components/App.js
+++ components/App.js
@@ -1,1 +1,9 @@
+import React, { Component } from 'react'
 
+var App =  React.createClass({
+    render: function()  {
+      return <div> This is definitely a React app now! Try updating.</div>
+    }
+});
+
+export default App
